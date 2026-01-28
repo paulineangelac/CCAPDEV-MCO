7028f4ab009c55bf33a7d76a0863b7b03a4954db
EJ <ejcunanan8@gmail.com>
2026-01-28 12:00:59 +0800

    fixed font

--- js/LoginPage.js
+++ js/LoginPage.js
@@ -6,7 +6,7 @@ loginForm.addEventListener('submit', function(event){
     const user = document.getElementById('username').value;
     const pass = document.getElementById('password').value;
 
-    
+
     if(user === 'admin' && pass === 'admin123'){
         console.log('Login successful');
         window.location.href = 'Student-Dashboard.html';
@@ -15,5 +15,4 @@ loginForm.addEventListener('submit', function(event){
         alert('Invalid username or password');
         loginForm.reset();
     }
-
 })
