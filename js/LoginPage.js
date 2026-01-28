1884e045b97b5beeba35cdcd23271cc835512504
EJ <ejcunanan8@gmail.com>
2026-01-28 11:18:55 +0800

    temp

--- js/LoginPage.js
+++ js/LoginPage.js
@@ -6,7 +6,7 @@ loginForm.addEventListener('submit', function(event){
     const user = document.getElementById('username').value;
     const pass = document.getElementById('password').value;
 
-
+    
     if(user === 'admin' && pass === 'admin123'){
         console.log('Login successful');
         window.location.href = 'Student-Dashboard.html';
