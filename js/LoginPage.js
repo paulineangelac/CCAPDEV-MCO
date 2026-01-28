b2fc4f6dc52f056238812785d2aa567aa98b4065
EJ <ejcunanan8@gmail.com>
2026-01-28 11:07:59 +0800

    added hard coded javascript for login

---
+++ js/LoginPage.js
@@ -0,0 +1,19 @@
+const loginForm = document.querySelector('form');
+
+loginForm.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const user = document.getElementById('username').value;
+    const pass = document.getElementById('password').value;
+
+
+    if(user === 'admin' && pass === 'admin123'){
+        console.log('Login successful');
+        window.location.href = 'Student-Dashboard.html';
+    }
+    else{
+        alert('Invalid username or password');
+        loginForm.reset();
+    }
+
+})
