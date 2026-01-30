d8a5ec1128350e5415b6f634ec8802ee648049df
EJ <ejcunanan8@gmail.com>
2026-01-30 16:54:47 +0800

    removed not needed

--- js/LoginPage.js
+++ js/LoginPage.js
@@ -1,29 +1,11 @@
-const records=[
-    {username: 'John Doe', bookings:[{room: 'Goks 304A', time: '08:00 AM', date: '2024-11-15'}], status:'Student'},
-    {username: 'Mary Jane', bookings:[], status:'Admin'}
-]
-
-localStorage.setItem('allRecords', JSON.stringify(records));
-
-const rooms = ['Goks 304A', 'Goks 304B', 'Goks 306A', 'Goks 306B', 'Andrew 1904'];
-localStorage.setItem('allRooms', JSON.stringify(rooms));
 /* Login Form Functionality */
 const loginForm = document.querySelector('form');
 
 loginForm.addEventListener('submit', function(event){
     event.preventDefault();
 
-    const user = document.getElementById('username').value;
-    const pass = document.getElementById('password').value;
-
-    if(records.some(record => record.username === user) && pass === 'admin123'){
-        localStorage.setItem('logedInUser', user);
-        window.location.href = 'Student-Dashboard.html';
-    }
-    else{
-        alert('Invalid username or password');
-        loginForm.reset();
-    }
+         
+    window.location.href = 'Student-Dashboard.html';
 })
 
 /* End of Login Form Functionality */
--- js/Logout.js
+++ js/Logout.js
@@ -1,15 +1,15 @@
+
+
 const logoutForm = document.querySelector('form')
 
 logoutForm.addEventListener('submit', function(event){
     event.preventDefault();
-    console.log("im here")
     logout();
 })
 
-/* End of Logout Functionality */
 
+/* Logout Functionality */
 function logout() {
-    localStorage.clear();
     window.location.href = 'LoginPage.html';
 }
 /* End of Logout Functionality */
