35a85047560fcd7c3f476cdaad8693c549e3b2bf
EJ <ejcunanan8@gmail.com>
2026-02-13 19:23:42 +0800

    fixed login

--- js/LoginPage.js
+++ js/LoginPage.js
@@ -1,10 +1,12 @@
 /* Login Form Functionality */
 const loginForm = document.querySelector('form');
-
+const usernameForm = document.getElementById('username');
 loginForm.addEventListener('submit', function(event){
     event.preventDefault();
-
-         
+    const username = usernameForm.value;
+     
+    if(username === "admin")window.location.href = 'AdminDashboardPage.html';
+    if(username === "labtech")window.location.href = 'LabTechDashboardPage.html';
     window.location.href = 'StudentDashboardPage.html';
 })
 
