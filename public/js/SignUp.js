58ef13fbbcabb88299fcf0a287395c5529978d92
pauline <pauline_novera@dlsu.edu.ph>
2026-03-30 18:15:53 +0800

    dlsu email validation

--- public/js/SignUp.js
+++
@@ -1,6 +0,0 @@
-const signUpButton = document.getElementById('submitButton');
-
-signUpButton.addEventListener('submit', function(event){
-    event.preventDefault();
-    window.location.href='LoginPage.html';
-})
